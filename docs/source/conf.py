51c0392cdf35166f05c28f8f762cdf8a7df5be0e
zmoon <zmoon92@gmail.com>
2025-02-03 16:18:25 -0600

    Towards docs building

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,38 +1,45 @@
-import os
-import sys
+project = 'GDAS CMAQ Preprocessor'
+copyright = '2025, NOAA Air Resources Laboratory'
+author = 'NOAA Air Resources Laboratory'
 
-# Add the project root directory and src directory to the Python path
-sys.path.insert(0, os.path.abspath('../..'))
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    "sphinx.ext.extlinks",
+    # "sphinx.ext.linkcode",
+    'myst_parser'
+]
 
+extlinks = {
+    "issue": ("https://github.com/noaa-oar-arl/gdas-cmaqprep/issues/%s", "GH"),
+    "pull": ("https://github.com/noaa-oar-arl/gdas-cmaqprep/pull/%s", "PR"),
+}
+
+autosummary_generate = True  # default in Sphinx v4
+autodoc_default_options = {
+    "members": True,
+    "special-members": "__init__",
+}
 # Configuration for autodoc
 autodoc_mock_imports = [
     'numpy',
     'xarray',
-    'netCDF4',
-    'yaml',
+    # 'netCDF4',
+    # 'yaml',
     'requests',
-    'tqdm'
+    # 'tqdm'
 ]
 
-# Add both project root and src directory to Python path
-sys.path.insert(0, os.path.abspath('../../src'))
-
-project = 'GDAS CMAQ Preprocessor'
-copyright = '2024'
-author = 'Author'
-
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
-]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Add any files you want to copy from the root to the docs
 html_extra_path = ['../../LICENSE']
